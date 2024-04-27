f5318f17fa732515d74b13269f1939f0237eeb32
Gordey908 <144526824+Gordey908@users.noreply.github.com>
2024-04-27 11:36:46 +0200

    lesson34

---
+++ FirstPerson/Assets/Scripts/Slot.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField]
+    private Transform tempParentForSlots;
+    [SerializeField]
+    private Text descriptionPanelText;
+    private PlayerController pController;
+    private string parentName;
+    public ItemData itemData;
+
+    private void Start()
+    {
+        parentName = transform.parent.name;
+    }
+
+    private void AddToListOnDrag(List<GameObjcet> slots, List<ItemData> items, Transform parent)
+    {
+        if (itemData == null) return;
+        {
+            if (itemData.isUniq || slots.Count == 0)
+            {
+                slots.Add(gameObject);
+                items.Add(itemData);
+                teansform.SetParent(parent);
+                parentName = transforrm.parent.name;
+            }
+            else if (!itemData.isUniq)
+            {
+                for (int i = 0; i < slots.Count; i++)
+                {
+                    if (slot[i].GetComponent<Slot>().itemData.id == itemData.id)
+                    {
+                        items[i].count += itemData;
+                        slots[i].transform.Find("ItemCountText").GetComonent<Text>().text = slots[i].GetComponent<Slot>().itemData.count.ToString();
+                        Destroy(gameObject);
+                        break;
+                    }
+                    else if(i == slots.Count - 1)
+                    {
+                        slots.Add(gameObject);
+                        items.Add(itemData);
+                        transform.SetParent(parent);
+                        parentName = transform.parent.name;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        InventoryManager.instance.GetDescriptionPanel().SetActive(false);
+        transform.SetParent(tempParentForSlots);
+        transform.position = eventData.position;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        var iManager = InventoryManager.instance;
+        iManager.GetDescriptionPanel().SetActive(true);
+        if(itemData != null)
+        {
+            descriptionPanelText.text = itemData.description;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        InventoryManager.instance.GetDescriptionPanel().SetActive(false);
+    }
+}
